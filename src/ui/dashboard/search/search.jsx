5bccc3ae8af1da5e2a8a993b86e95e6076ed8bbd
Bhzd <bhzd.bakhshayesh@gmail.com>
2024-07-03 17:17:15 +0430

    .

--- src/ui/dashboard/search/search.jsx
+++ src/ui/dashboard/search/search.jsx
@@ -2,11 +2,22 @@
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import styles from "./search.module.css";
 import { MdDashboard } from "react-icons/md";
+import { useDebouncedCallback } from "use-debounce";
 
 const Search = ({ placeholder }) => {
   const pathName = usePathname();
   const serchParams = useSearchParams();
   const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((e) => {
+    const params = new URLSearchParams(serchParams);
+    if (e.target.value) {
+      e.target.value.length > 2 && params.set("q", e.target.value);
+    } else {
+      params.delete("q");
+    }
+    replace(`${pathName}?${params}`);
+  }, 300);
 
   return (
     <div className={styles.container}>
@@ -15,15 +26,7 @@ const Search = ({ placeholder }) => {
         type="text"
         placeholder={placeholder}
         className={styles.input}
-        onChange={(e) => {
-          const params = new URLSearchParams(serchParams);
-          if (e.target.value) {
-            params.set("q", e.target.value);
-          } else {
-            params.delete("q");
-          }
-          replace(`${pathName}?${params}`);
-        }}
+        onChange={handleSearch}
       />
     </div>
   );
