7f3261a8f436a37d8b3ada5fdbd9281702ef868d
Bhzd <bhzd.bakhshayesh@gmail.com>
2024-07-03 16:53:38 +0430

    .

--- src/app/dashboard/users/page.js
+++ src/app/dashboard/users/page.js
@@ -1,10 +1,13 @@
+import { fetchUsers } from "@/lib/data";
 import Pagination from "@/ui/dashboard/pagination/pagination";
 import Search from "@/ui/dashboard/search/search";
 import styles from "@/ui/dashboard/users/users.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-const page = () => {
+const page = async () => {
+  const users = await fetchUsers();
+  console.log({ users });
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -25,34 +28,36 @@ const page = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div className={styles.user}>
-                <Image
-                  src="/noavatar.png"
-                  alt=""
-                  width={40}
-                  height={40}
-                  className={styles.userImage}
-                />
-                John Doe
-              </div>
-            </td>
-            <td>John@gmail.com</td>
-            <td>13.01.2022</td>
-            <td>Admin</td>
-            <td>Active</td>
-            <td>
-              <Link href="/dashboard/users/123">
-                <button className={`${styles.button} ${styles.view}`}>
-                  View
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={user.img ?? "/noavatar.png"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>{user?.createdAt?.toString().slice(4, 16)}</td>
+              <td>{user.isADMIN ? "Admin" : "client"}</td>
+              <td>{user.isActive ? "Active" : "passive"}</td>
+              <td>
+                <Link href={`/dashboard/users/${user.id}`}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
                 </button>
-              </Link>
-              <button className={`${styles.button} ${styles.delete}`}>
-                Delete
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <Pagination />
--- src/ui/dashboard/search/search.jsx
+++ src/ui/dashboard/search/search.jsx
@@ -1,11 +1,26 @@
+"use client";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import styles from "./search.module.css";
 import { MdDashboard } from "react-icons/md";
 
 const Search = ({ placeholder }) => {
+  const pathName = usePathname();
+  const serchParams = useSearchParams();
+  const { replace } = useRouter();
+
   return (
     <div className={styles.container}>
       <MdDashboard />
-      <input type="text" placeholder={placeholder} className={styles.input} />
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={styles.input}
+        onChange={(e) => {
+          const params = new URLSearchParams(serchParams);
+          params.set("q", e.target.value);
+          replace(`${pathName}?${params}`);
+        }}
+      />
     </div>
   );
 };
